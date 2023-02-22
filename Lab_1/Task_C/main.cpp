f3d7d96ab332fc2b1bdce47e78f6e6d1ab33d489
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2023-02-22 17:50:29 +0300

    Task_C was added(working version)

--- Lab_1/Task_C/main.cpp
+++ Lab_1/Task_C/main.cpp
@@ -1,58 +1,73 @@
 #include <iostream>
 #include <cmath>
 
-void readStrings(std::string strings[], int n){
-    for (int i = 0; i < n; ++i) {
-        std::cin >> strings[i];
+using namespace std;
+
+int controlBitQuantity(int strLength){
+    int count = 0;
+    while (pow(2, count) < strLength){
+        count++;
+    }
+    return count + 1;
+}
+
+bool isDegreeOfTwo(int value){
+    int count = 0;
+    for (int i = 0; i < 32; ++i) {
+        if ((value >> i) & 1) {
+            count++;
+        }
+    }
+    if (count > 1){
+        return false;
+    } else {
+        return true;
     }
 }
 
 int main() {
     int strCount;
-    std::cin >> strCount;
-
-    std::string stringsInput[strCount];
-    readStrings(stringsInput, strCount);
+    cin >> strCount;
 
     for (int i = 0; i < strCount; ++i) {
-        std::string str = stringsInput[i];
-        int controlBitCount = log2(str.length()) + 1;
-        int mistakeIndex = -1;
-        int controlBitValue = 1; //Значение котрольного бита
+        string str;
+        cin >> str;
+        int controlBitAmount = controlBitQuantity(str.length());
+        int controlBitValue = 1;
+        int mistakeIndex = 0;
 
-        for (int j = 0; j < controlBitCount; ++j) {
-            int oneCount = 0;
-
-            for (int k = controlBitValue - 1; k < str.length(); k += controlBitValue * 2) {
-                int index = k;
-                for (int l = 0; l < controlBitValue; ++l) {
-                    if (str[index++] == '1') oneCount++;
+        while (controlBitValue < pow(2, controlBitAmount)){
+            int count = 0;
+            int iterator = 0;
+            for (int j = controlBitValue - 1; j < str.length(); ++j) {
+                if (str[j] == '1'){
+                    count++;
+                }
+                iterator++;
+                if (iterator == controlBitValue){
+                    j += iterator;
+                    iterator = 0;
                 }
             }
-
-            if (oneCount % 2 != 0){
+            if (count % 2){
                 mistakeIndex += controlBitValue;
             }
+
             controlBitValue *= 2;
         }
 
-        if (mistakeIndex > -1) {
-            if (str[mistakeIndex] == '0') {
-                str[mistakeIndex] = '1';
-            } else if (str[mistakeIndex] == '1'){
-                str[mistakeIndex] = '0';
-            }
+        if (mistakeIndex){
+            str[mistakeIndex - 1] = str[mistakeIndex - 1] == '0' ? '1' : '0';
         }
 
         for (int j = 0; j < str.length(); ++j) {
-            double temp;
-            if (modf(log2(j + 1), &temp) > 0) {
-                std::cout << str[j];
+            if (isDegreeOfTwo(j + 1)){
+                continue;
             }
+            cout << str[j];
         }
-        std::cout << "\n";
+        cout << "\n";
 
     }
-
     return 0;
 }
