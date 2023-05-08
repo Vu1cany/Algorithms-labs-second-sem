1319a6b154c54e8dbb3333a820c1224e1eebab06
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2023-05-08 10:10:41 +0300

    Final version

--- Lab_5/Task_A/main.cpp
+++ Lab_5/Task_A/main.cpp
@@ -1,6 +1,88 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Edge{
+    int start;
+    int end;
+    int weight;
+
+    Edge(int start, int anEnd, int weight) : start(start), end(anEnd), weight(weight) {};
+    Edge() = default;
+
+    bool operator < (Edge edge) const{
+        return weight < edge.weight;
+    }
+};
+
+struct DSU{
+    int* parent;
+    int* size;
+
+    DSU(int n){
+        this->parent = new int[n];
+        this->size = new int[n];
+        for (int i = 0; i < n; ++i) {
+            parent[i] = 0;
+            size[i] = 0;
+        }
+    }
+
+    void makeSet(int v){
+        if (parent[v] == 0){
+            parent[v] = v;
+            size[v] = 1;
+        }
+    }
+
+    int findSet(int v){
+        if (v == parent[v]){
+            return v;
+        } else {
+            return parent[v] = findSet(parent[v]);
+        }
+    }
+
+    void unionSets(int v, int u){
+        int x = findSet(v);
+        int y = findSet(u);
+        if (x != y) {
+            if (size[x] < size[y])
+                swap (x, y);
+            parent[y] = x;
+            size[x] += size[y];
+        }
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int N, M;
+    cin >> N >> M;
+    N++;
+    vector<Edge> edges(M);
+    for (int i = 0; i < M; ++i) {
+        int start, end, weight;
+        cin >> start >> end >> weight;
+        edges[i] = Edge(start, end, weight);
+    }
+
+    sort(edges.begin(), edges.end());
+
+    DSU dsu = DSU(N);
+    for (int i = 1; i < N; ++i) {
+        dsu.makeSet(i);
+    }
+
+    int result = 0;
+    for (Edge edge : edges) {
+        if (dsu.findSet(edge.start) != dsu.findSet(edge.end)){
+            result += edge.weight;
+            dsu.unionSets(edge.start, edge.end);
+        }
+    }
+
+    cout << result;
+
     return 0;
 }
